2c1163a552782df15e777609bd086c6dfa036f41
ramajeyam-clayfin <ramajeyam@clayfin.com>
2022-07-14 09:29:55 +0530

    fonts adding

---
+++ src/Constants/Fonts.js
@@ -0,0 +1,3 @@
+export default {
+  Bold: 'PoppinsBold-GdJA',
+};
--- src/Containers/Login/LoginPage.js
+++ src/Containers/Login/LoginPage.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import LinearGradient from 'react-native-linear-gradient';
+import {actuatedNormalize} from '../../Constants/PixelScaling';
+import Fonts from '../../Constants/Fonts';
 
 const LoginPage = () => {
   return (
-    <View>
-      <Text>LoginPage</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      colors={['#E8F3DD', '#BCC5D2', '#C0AEC3']}
+      style={{
+        flex: 1,
+      }}>
+      <View>
+        <Text
+          style={{
+            fontSize: actuatedNormalize(24),
+            fontFamily: Fonts.Bold,
+          }}>
+          Hello Gamer!
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
