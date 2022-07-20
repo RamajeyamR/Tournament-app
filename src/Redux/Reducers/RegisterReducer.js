973e65453e51dff9d46c9516133af27678ec82d5
RamajeyamR <jeyamjeyam6@gmail.com>
2022-07-20 13:13:13 +0530

    reducer

--- src/Redux/Reducers/RegisterReducer.js
+++ src/Redux/Reducers/RegisterReducer.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
 export const RegisterReducer = createSlice({
   name: 'Register',
   initialState: {
@@ -40,79 +39,10 @@ export const RegisterReducer = createSlice({
         isEmail : true
       }
     },
-    // password : {
-    //   field : 'password',
-    //   value: '',
-    //   errorMsg : '',
-    //   valid : false,
-    //   touched : false,
-    //   validationRules : {
-    //     isRequired : true,
-    //     minLength: 8,
-    //   }
-    // },
-    // confirmPassword : {
-    //   field : 'confirmPassword',
-    //   value: '',
-    //   errorMsg : '',
-    //   valid : false,
-    //   touched : false,
-    //   validationRules : {
-    //     isRequired : true,
-    //     minLength: 8,
-    //   }
-    // }
   },
   
   reducers: {
-    updateName: (state, action) => {
-      state.name = {
-        ...state.name,
-        value : action.payload,
-        touched : true,
-        errorMsg : '',
-      }
-    },
-    updateMobile: (state, action) => {
-      if (isNaN(action.payload) ){
-        console.log('NAN')
-        state.mobile = {
-          ...state.mobile,
-          value : state.mobile.value,
-          touched : true,
-          errorMsg : '',
-        }
-      }else state.mobile = {
-        ...state.mobile,
-        value : action.payload,
-        touched : true,
-        errorMsg : '',
-      }
-    },
-    updateEmail: (state, action) => {
-      state.email = {
-        ...state.email,
-        value : action.payload,
-        touched : true,
-        errorMsg : '',
-      }
-    },
-    updatePassword: (state, action) => {
-      state.password = {
-        ...state.password,
-        value : action.payload,
-        touched : true,
-        errorMsg : '',
-      }
-    },
-    updateconfirmPassword: (state, action) => {
-      state.confirmPassword = {
-        ...state.confirmPassword,
-        value : action.payload,
-        touched : true,
-        errorMsg : '',
-      }
-    },
+
     validations : (state, action) => {
       // console.log('Actions-->',action)
       state[action.payload.name] = {
@@ -131,6 +61,6 @@ export const RegisterReducer = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { onChange, updateName, updateMobile, updateEmail, updatePassword, updateconfirmPassword, resetState, validations } = RegisterReducer.actions
+export const { onChange, validations } = RegisterReducer.actions
 
 export default RegisterReducer.reducer
